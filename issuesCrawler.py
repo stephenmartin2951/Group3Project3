bf1227a1aa6e210d4f69fa9aa85f92b302e67d53
stephenmartin2951 <stephenmartin2951@pm.me>
2023-03-19 20:03:24 -0500

    Renamed issues crawler and parameterized it
    -o --owner for repo owner and -r --repo for repo name. Will automatically write issues data to .csv

---
+++ issuesCrawler.py
@@ -0,0 +1,45 @@
+from ghapi.all import GhApi
+import csv
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-o", "--owner", type=str)
+parser.add_argument("-r", "--repo", type=str)
+args=parser.parse_args()
+GHOwner = args.owner
+GHRepo = args.repo
+
+if(GHOwner == None):
+    GHOwner = 'PaperMC'
+
+if(GHRepo == None):
+    GHRepo = 'Paper'
+
+
+github_token = "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"
+
+api = GhApi(owner=GHOwner, repo=GHRepo, token=github_token, ref='heads/master')
+
+def populateIssueData():  
+    pageNum = 1
+    issues = api('/repos/{}/{}/issues'.format(GHOwner, GHRepo), 'GET', query=dict(state='all', per_page=100, page=pageNum))
+    with open('{}Issues.csv'.format(GHRepo), 'w', newline='') as csvfile:
+        issuewriter = csv.writer(csvfile)
+        issuewriter.writerow(["State", "Labels", "Assignees", "Comments", "Closed_At", "Created_At", "Locked"])
+        #iterate through every issue
+        while(len(issues) > 0):
+            #Write each issue to csv
+            for issue in issues:
+                issuewriter.writerow([issue.state, issue.labels, len(issue.assignees), issue.comments, issue.closed_at, issue.created_at, issue.locked])
+            pageNum += 1
+            issues = api('/repos/{}/{}/issues'.format(GHOwner, GHRepo), 'GET', query=dict(state='all', per_page=100, page=pageNum))  
+
+
+populateIssueData()
+
+#TODO: We can now populate data. We can import CSV into whichever tool we use to create visualization
+
+
+
+
+
