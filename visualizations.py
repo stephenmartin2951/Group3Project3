e4d5f27c39c617d65873ae781cf7b3c6d18a867c
stephenmartin2951 <stephenmartin2951@pm.me>
2023-03-25 13:23:39 -0500

    Polymorphed scatterplot to compare two repos

--- visualizations.py
+++ visualizations.py
@@ -10,7 +10,14 @@ def createScatterPlot(df, xAxis, yAxis, color):
     df.plot.scatter(x = xAxis, y = yAxis, c = color)
     plt.show()
 
+#Comparing two repos
+def createScatterPlot(df, xAxis, yAxis, color, df2, color2):
+    df.plot.scatter(x = xAxis, y = yAxis, c = color)
+    df2.plot.scatter(x = xAxis, y = yAxis, c = color2)
+    plt.show()    
+
 
 df = createDataFrame('issues/PaperMCPaperIssues.csv')
+df2 = createDataFrame('issues/bakkesmodorgBakkesModSDKIssues.csv')
 
-createScatterPlot(df, 'Created_At', 'Comments', 'DarkBlue')
+createScatterPlot(df, 'Created_At', 'Comments', 'DarkBlue', df2, "Green")
